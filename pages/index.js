46c14bd8e11615a000745a6096343d3d8bdee62c
Lucas Mantuan <lucasmantuan@outlook.com>
2023-07-13 09:59:01 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-    return <h1>Camis, eu te amo!!!</h1>;
+  return <h1>Camis, eu te amo!!!</h1>;
 }
 
 export default Home;
